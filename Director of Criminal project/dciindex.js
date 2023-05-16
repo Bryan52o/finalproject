dc61059452153c2632df3c37a42224392d24f731
Bryan52o <bryanwere335@gmail.com>
2023-05-16 17:22:18 +0300

    initial commit

---
+++ Director of Criminal project/dciindex.js
@@ -0,0 +1,40 @@
+console.log("welcom to kenya DCI");
+
+let bribery =[
+    "bryan ","dickson "
+];
+if (bribery.includes("bryan")){
+alert("You have a bribery case to Answer")};
+console.log(bribery);
+//console.log("You have a bribery case to Answer");
+document.getElementById("count-el").innerText= 0
+let count=0
+ function increment(){
+    count=count=+1;
+    console.log(count);
+ }
+ increment();
+ function testing(){
+    console.log("learning functions in javascript");
+    console.log("its not easy but am optimistic");
+ }
+ testing();
+ testing();
+ testing();
+
+ function numbering(){
+console.log("4")
+ }
+ numbering();
+ let lap1=3
+ let lap2=4
+ let lap3=25
+  function adding(){
+    console.log(lap1+lap2 +lap3);
+  }
+  adding();
+  let lapscompleted=0
+  function incrementing(){
+    lapscompleted= lapscompleted+1
+  }
+  incrementing();
---
+++ final project/index.js
@@ -0,0 +1,53 @@
+console.log("trying to write javascript")
+let dailySalary=1500;
+let weeklysalary= 1500*6;
+console.log(weeklysalary);
+const firstName="bryan";
+const secondName="engineer";
+const fullName = ( "my names are " + firstName  +" " + secondName );
+
+console.log(fullName)
+
+ const skyIsBlue= true;
+ if(2+2==3){
+    console.log('all this section should be true')
+    console.log("the sky is blue");
+ }
+    else {
+   
+console.log("the sky is not blue");
+console.log("and that maths is not mathing")
+ }
+ const marks= 49;
+ if(marks==50){ 
+    console.log("you have qualified");}
+    else if(marks<50){
+        console.log("try again next time")
+    }
+    else{ 
+        console.log("never come heere again");
+    }
+    const arsenal = "the pguyDTv oF loNdon";
+    const toLowerTheCity= arsenal.toUpperCase();
+    console.log(toLowerTheCity);
+    const changingCase = toLowerTheCity.toLowerCase();
+    console.log(changingCase);
+    console.log(Date.now());
+    
+    console.error("dont try this at home bbe");
+    console.warn("dont try this at home bbe");
+    console.info("hi");
+    const introduction = document.querySelector(".intro");
+    introduction.style.backgroundColor="";
+    function sayHello(){
+        alert("hello my goodpeople")
+    }
+     console.warn("enter your name");
+let messege ="please enter your full details"
+     const button =document.querySelector(".butt");
+     button.addEventListener("click",function(){
+prompt(messege);
+     } );
+
+    
+ 
